f2ede3dcb5e4ccb7381832131a81bfadbe33d92d
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2018-08-27 15:34:43 +0300

    initial commit

---
+++ PythonServer/button.py
@@ -0,0 +1,11 @@
+
+from image import Image
+
+
+
+class Button:
+
+    icon = Image()
+
+    def pressed(self):
+        pass
---
+++ PythonServer/main.py
@@ -0,0 +1,32 @@
+# import the library
+from appJar import gui
+
+# handle button events
+def press(button):
+    if button == "Cancel":
+        app.stop()
+    else:
+        usr = app.getEntry("Username")
+        pwd = app.getEntry("Password")
+        print("User:", usr, "Pass:", pwd)
+
+# create a GUI variable called app
+app = gui("Login Window", "400x200")
+app.setBg("orange")
+app.setFont(18)
+
+# add & configure widgets - widgets get a name, to help referencing them later
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "blue")
+app.setLabelFg("title", "orange")
+
+app.addLabelEntry("Username")
+app.addLabelSecretEntry("Password")
+
+# link the buttons to the function called press
+app.addButtons(["Submit", "Cancel"], press)
+
+app.setFocus("Username")
+
+# start the GUI
+app.go()
